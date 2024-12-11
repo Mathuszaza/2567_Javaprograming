dd0cd76100b6e14a4d4db52d40c1e169a6a20635
Mathuszaza <srichatmethus@gmail.com>
2024-12-11 10:37:53 +0700

    Labsheet03

--- Labsheet03/src/ex.java
+++ Labsheet03/src/ex.java
@@ -1,9 +1,29 @@
-
+import java.util.*;
 public class ex {
-
+ 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		//กำหนดตัวแปรชื่อtextไว้รวมโปรแกรมเข้าด้วยกัน
+		String txtConcate = "";
+		//กำหนดตัวแปรชื่อ word ไว้สำหรับรับข้อความทาง Console
+		String word;
+		
+		while(true) {
+			System.out.print("Enter word: ");
+			word = input.next();
+			//ตรววจสอบเงื่อนไขเพื่อให้หยุดการรับข้อความ
+			if (word.equalsIgnoreCase("stop")  ) {
+				System.out.println("Program Terminate");
+				break;
+			}
+			//การรวมข้อความที่ป้อนเข้าด้วยกัน
+			txtConcate += word + " ";
+		}//end of while
+		System.out.println(txtConcate.toUpperCase());
+		
+		input.close();
+ 
 	}
-
+ 
 }
