edd5beaf6a9b54100018bd28d4adde18649e3142
Mathuszaza <srichatmethus@gmail.com>
2025-01-15 08:35:41 +0700

    Attendance

--- Labsheet04/src/Lab402.java
+++ Labsheet04/src/Lab402.java
@@ -1,9 +1,47 @@
-
+import javax.swing.*;
+ 
 public class Lab402 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        
+        //input
+        int normalYear = Integer.parseInt(JOptionPane.showInputDialog(null, "Input year:"));
+ 
+        
+        if (checkYear(normalYear) == false) {
+            while (true) {
+            	normalYear = Integer.parseInt(JOptionPane.showInputDialog(null, "Input year:"));
+                if (checkYear(normalYear) == false) {
+                    break;
+                }
+            }
+        }
+        
+        if (isLeapYear(normalYear) == true) {
+            JOptionPane.showMessageDialog(null, normalYear + " is Leap year.");
+        }
+        else {
+            JOptionPane.showMessageDialog(null, normalYear + " is NOT Leap year.");
+        }
+ 
+    }
+    
+    public static boolean isLeapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        }
+        else if (year % 100 == 0 && year % 400 == 0) {
+            return true;
+        }
+ 
+        return false;
+ 
+    }
+    
+    public static boolean checkYear(int checkYear) {
+        if (checkYear >= 1000 && checkYear <= 3000) {
+                
+            return true;
+        }
+        return false;
+    }
 }
