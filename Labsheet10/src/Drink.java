e46f510d9382ce460bc78d2749a756670f1ca520
Mathuszaza <srichatmethus@gmail.com>
2025-02-26 08:57:46 +0700

    Labsheet10

--- Labsheet10/src/Drink.java
+++ Labsheet10/src/Drink.java
@@ -11,56 +11,52 @@ public class Drink {
 		this(0,' ');
 	}
 	public String getTypeName() {
-		if(this.type == 1) {
-			return "Hot";
-		}
-		else if(this.type == 2) {
-			return "Cold";
-		}
-		else {
-			return null;
-		}
-	}
-	public int getTypePrice() {
-		if(this.type == 1) {
-			return 10;
-		}
-		else if(this.type == 2) {
-			return 20;
-		}
-		else {
-			return 0;
-		}
-	}
-	public String getSizePrice() {
-		if(this.size == 's'||this.type == 'S') {
-			return "Small";
-		}
-		else if(this.type == 'm'||this.type == 'M') {
-			return "Medium";
-		}
-		else if(this.type == 'l'||this.type == 'L') {
-			return "Large";
-		}
-		else {
-			return null;
-		}
-	}
-	public int getSizePrice() {
-		if(this.size == 's'||this.type == 'S') {
-			return 15;
-		}
-		else if(this.type == 'm'||this.type == 'M') {
-			return 20;
-		}
-		else if(this.type == 'l'||this.type == 'L') {
-			return 25;
-		}
-		else {
-			return 0;
-		}
-	}
-	public int getTotalPrice() {
-		
-	}
+        if (this.type == 1) {
+            return "Hot";
+        }
+        else if (this.type == 2){
+            return "Cold";
+        }
+        return null;
+       
+        }
+   
+    int getTypePrice() {
+        if (this.type == 1) {
+            return 10;
+        }
+        else if (this.type == 2){
+            return 20;
+        }
+        return 0;
+    }
+    String getSizeName() {
+        if (this.size == 's' || this.size == 'S') {
+            return "Small";
+        }
+        else if (this.size == 'm' || this.size == 'M') {
+            return "Medium";
+        }
+        else if (this.size == 'l' || this.size == 'L'){
+            return "Large";
+        }
+        return null;
+    }
+    int getSizePrice() {
+        if (this.size == 's' || this.size == 'S') {
+            return 15;
+        }
+        else if (this.size == 'm' || this.size == 'M') {
+            return 20;
+        }
+        else if (this.size == 'l' || this.size == 'L'){
+            return 25;
+        }
+        return 0;
+    }
+    int getTotalPrice() {
+        return getTypePrice() + getSizePrice();
+    }
+ 
+ 
 }
